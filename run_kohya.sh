4352cc3e9175bea0e1e200eb08f1215e835ebcb0
oztechin <enioztekin@gmail.com>
2025-11-28 08:23:04 +0300

    Add files via upload

--- run_kohya.sh
+++ run_kohya.sh
@@ -2,4 +2,9 @@ python /workspace/kohya_ss/setup_accelerate.py
 cd /workspace/kohya_ss
 source venv/bin/activate
 unset LD_LIBRARY_PATH
-./gui.sh --listen=0.0.0.0 --share --noverify
+
+# LOG DOSYASI
+LOGFILE="/workspace/kohya_gui.log"
+
+# GUI + LOG
+./gui.sh --listen=0.0.0.0 --share --noverify --port 7860 2>&1 | tee -a "$LOGFILE"
