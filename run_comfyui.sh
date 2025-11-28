48d511399c6fe6ec168e8d0f3841ef68b8ab703f
oztechin <enioztekin@gmail.com>
2025-11-28 06:57:20 +0300

    Add files via upload

--- run_comfyui.sh
+++ run_comfyui.sh
@@ -2,4 +2,4 @@ apt install psmisc
 cd /workspace/ComfyUI/venv
 source bin/activate
 cd /workspace/ComfyUI
-python main.py --listen 0.0.0.0 --port 7860 --use-sage-attention
+python main.py --listen 0.0.0.0 --port 6006 --use-sage-attention
