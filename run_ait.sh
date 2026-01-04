65e44940927fc7e28068b98598074045f0bae8f0
oztechin <enioztekin@gmail.com>
2026-01-04 07:10:38 +0300

    Update run_ait.sh

--- run_ait.sh
+++ run_ait.sh
@@ -1,7 +1,15 @@
 cd /workspace/ai-toolkit
 #python -m venv venv
 source venv/bin/activate
-git pull
+#git pull
+#pip install -r requirements.txt
+#cd ui
+#npm run build_and_start 2>&1 | tee -a "/workspace/ait.log"
+
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# UI
 cd ui
+npm install
 npm run build_and_start 2>&1 | tee -a "/workspace/ait.log"
