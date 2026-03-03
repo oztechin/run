207852adfd19fa8df85cd63bfafd9d7f51fc85ee
oztechin <enioztekin@gmail.com>
2026-03-03 18:01:18 +0300

    Update run_comfyui.sh

--- run_comfyui.sh
+++ run_comfyui.sh
@@ -37,5 +37,5 @@ git pull
 cd /workspace/ComfyUI
 source venv/bin/activate
 pip install -r requirements.txt
-
+ln -s /models   /workspace/ComfyUI/models/latent_upscale_models/
 python main.py --listen 0.0.0.0 --port 6006 --use-sage-attention
